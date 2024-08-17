e9b886e4e3262d43637a97934b80e325051dc38b
pujaroy200 <166097365+pujaroy200@users.noreply.github.com>
2024-08-17 01:35:35 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("spo","spo")
+        ERC721("nn","nn")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
